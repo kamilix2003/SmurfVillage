d15d8097a77e0ae0f1ac0c27659387fb943e1a1d
Kamil Chaj <kamaj2309@gmail.com>
2025-03-13 22:25:34 +0100

    Dodaj pliki projektów.

---
+++ SmurfVillage/Creature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmurfVillage
+{
+    class Creature
+    {
+        string name;
+        uint age;
+        bool gender;
+    }
+}
